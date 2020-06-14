5a0a035242c0db7bb346063d93d205bc63d7ae55
kenny-kerson <kenny.kerson@gmail.com>
2020-06-14 15:08:09 +0900

    1. domain-rabbitmq 모듈 추가

---
+++ domains/domain-rabbitmq/src/main/java/com/kenny/mm/domainrabbitmq/DomainRabbitmqApplication.java
@@ -0,0 +1,13 @@
+package com.kenny.mm.domainrabbitmq;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DomainRabbitmqApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DomainRabbitmqApplication.class, args);
+    }
+
+}
